8a64f862fdc93faf490df693e41e66659e1b88da
Antonio Santos <aj.santos128@gmail.com>
2018-12-30 01:52:58 -0400

    Changes properties for Forecast Object.

--- src/forecast.js
+++ src/forecast.js
@@ -1,83 +1,20 @@
-/*
- * Represents a weather forecast.
- */
-class Forecast {
-	/**
-	 * Create a weather forecast.
-	 * @param {number} temperature - The current temperature
-	 * @param {string} address - The location weather is being displayed for.
-	 * @param {number} precipitation - The chance of rain.
-	 * @param {string} icon - The icon that describes the current weather.
-	 * @param {number} windSpeed - The current wind speed.
-	 * @param {number} windDirection - The direction the wind is blowing in degrees.
-	 * @param {string} summary - A summary of the current weather conditions.
-	 * @param {number} high - The day's high temperatue.
-	 * @param {number} low - The day's low temperature.
-	 */
-	constructor(temperature, address, precipitation, icon, windSpeed, windDirection, summary, high, low) {
-		/* The following if statements verify that the given arguments are valid. */
-		if (!isNumber(temperature)) {
-			throw new SyntaxError('temperature is not a Number.');
-		}
-
-		if (!isNumber(precipitation)) {
-			throw new SyntaxError('precipitation is not a Number.');
-		}
-
-		if (!isString(icon)) {
-			throw new SyntaxError('icon is not a String');
-		}
-
-		if (!isNumber(windSpeed)) {
-			throw new SyntaxError('windSpeed is not a Number.');
-		}
-
-		if (!isNumber(high)) {
-			throw new SyntaxError('high is not a Number.');
-		}
-
-		if (!isNumber(low)) {
-			throw new SyntaxError('low is not a Number.');
-		}
-
-		if(!isString(address)) {
-			throw new SyntaxError('address is not a String.');
-		}
-
-		if (!isNumber(windDirection)) {
-			throw new SyntaxError('windDirection is not a Number.');
-		}
-
-		if (!isString(summary)) {
-			throw new SyntaxError('summary is not a String.');
-		}
-
-		this.temperature = temperature;
-		this.address = address;
-		this.precipitation = precipitation;
-		this.icon = icon;
-		this.windSpeed = windSpeed;
-		this.windDirection = degreesToCompass(windDirection);
-		this.summary = summary;
-		this.high = high;
-		this.low = low;
-	}
-}
-
-/** 
- * Validate that the given value is of type Number.
- * @param {number} value - The number to verify. 
- */
-function isNumber(value) {
-	return typeof value === 'number' && isFinite(value);
-}
-
-/** 
- * Validate that the given value is of type String.
- * @param {string} value - The String `to verify. 
- */
-function isString(value) {
-	return typeof value === 'string' || value instanceof String;
+function Forecast(time, summary, icon, precipIntensity, precipProbability, 
+				temperature, apparentTemperature, humidity, pressure, windSpeed,
+				windBearing, cloudCover, UVIndex, visibility) {
+	this.time = time;
+	this.summary = summary;
+	this.icon = icon;
+	this.precipIntensity = precipIntensity;
+	this.precipProbability = precipProbability;
+	this.temperature = temperature;
+	this.apparentTemperature = apparentTemperature;
+	this.humidity = humidity;
+	this.pressure = pressure;
+	this.windSpeed = windSpeed;
+	this.windBearing = windBearing;
+	this.cloudCover = cloudCover;
+	this.UVIndex = UVIndex;
+	this.visibility = visibility;
 }
 
 /* Snippet taken from https://stackoverflow.com/a/48750814 */
